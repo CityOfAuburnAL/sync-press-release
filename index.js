9f14e7c02252eff28a612e821ccd9a2fd7c70228
cgriffin@auburnalabama.org <cgriffin@auburnalabama.org>
2019-06-30 17:12:39 -0500

    Found out I've got no plans.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Here');
+const fetch = require('node-fetch');
+
+// Need to use only 'current' press releases for the real thing
+// Need to make this work for multiple front pages and lists
+// Need to make this where it can be executed from the web (like my-press-release)
+fetch('https://api2.auburnalabama.org/pressrelease/join/list/3/active')
+    .then((res) => res.json())
+    .then((articles)=> {
+        console.log(articles);
+    });
+
+fetch('https://api.auburnalabama.org/cms/sitepage/9/region')
+    .then((res) => res.json())
+    .then((regions) => {
+        console.log(regions);
+    });
